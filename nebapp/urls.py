c2960d3ca6240f9c8027d162bb9dd8e9b297e62a
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-02-04 18:21:10 +0200

    add changes

---
+++ nebapp/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from tinymce.models import HTMLField
+
+
+urlpatterns=[
+    url(r'^$', views.welcome, name="welcome"),
+    url(r'^profile/$', views.user_profile, name='user-profile'),
+    url(r'^edit/profile/$', views.edit_profile, name="edit-profile"),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new/hood$', views.create_hood, name='new-hood'),
+    url(r'^addbusiness/(\d+)$', views.new_bussiness, name='new-business'),
+    # url(r'^exithood/(\d+)$', views.exit_hood, name='exit-hood'),
+    url(r'^addpost/(\d+)$', views.new_post, name='new-post'),
+    url(r'^location$', views.location, name='location'),
+    url(r'^myhood/(\d+)', views.my_hood, name='my-hood'),
+    url(r'^comment/(\d+)',views.new_comment, name='new-comment'),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
